c5eac0b21b05f461ebda893d7e836328b29dbea2
Kyzyl-ool <kyzyloolk@mail.ru>
2018-05-01 12:26:38 +0300

    Первая

---
+++ Arcanoid/Brick.cpp
@@ -0,0 +1,9 @@
+//
+//  Brick.cpp
+//  Arcanoid
+//
+//  Created by Кежик Кызыл-оол on 01.05.2018.
+//  Copyright © 2018 Кежик Кызыл-оол. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ Arcanoid/Support.cpp
@@ -0,0 +1,16 @@
+//
+//  Support.cpp
+//  Arcanoid
+//
+//  Created by Кежик Кызыл-оол on 01.05.2018.
+//  Copyright © 2018 Кежик Кызыл-оол. All rights reserved.
+//
+
+class abstract_support
+{
+    int x, y;   //coordinates
+    int length;
+    int height;
+    
+    
+};
