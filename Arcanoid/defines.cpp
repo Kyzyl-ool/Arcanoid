75afc09d2c582b40fe5ea06443cb475ca70348c5
Kyzyl-ool <kyzyloolk@mail.ru>
2018-05-10 04:34:33 +0300

    Коммит

---
+++ Arcanoid/defines.cpp
@@ -0,0 +1,38 @@
+//
+//  defines.cpp
+//  Arcanoid
+//
+//  Created by Кежик Кызыл-оол on 10.05.2018.
+//  Copyright © 2018 Кежик Кызыл-оол. All rights reserved.
+//
+
+#define DEFAULT_BLOCKS_FILE "default_blocks.png"
+#define BLOCKS_AND_BALLS_FILE "blocks2.png"
+#define BACKGROUNDS_FILE "backgrounds.png"
+#define BOARD_FILE "support.png"
+
+
+#define BALL_SIZE 44
+
+#define AMOUNT_OF_GAME_OBJECTS 1000
+#define GAME_SLOWLESS 20000
+
+#define AMOUNT_OF_GAME_OBJECTS 1000
+
+#define MAX_BLOCKS_X 13
+#define MAX_BLOCKS_Y 10
+
+#define BLOCK_HEIGHT 30
+#define BLOCK_WIDTH 60
+
+
+#define SUPPORT_HEIGHT 22
+#define SUPPORT_EPSILON 4
+
+#define DEFAULT_SCREEN_WIDTH 800
+#define DEFAULT_SCREEN_HEIGHT 600
+
+#define BACKGROUND_TEXTURE_SIZE 60
+#define BACKGROUND_DISTANCE 2
+#define BACKGROUND_BORDER_THICKNESS 10
+
